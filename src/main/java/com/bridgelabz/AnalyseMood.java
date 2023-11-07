a218d22692dff0f01c7562cba024922cf5897849
Rohan <rohandute1@gmail.com>
2023-11-07 12:06:17 +0530

    Added:- successfully return sad when message contains happy

--- src/main/java/com/bridgelabz/AnalyseMood.java
+++ src/main/java/com/bridgelabz/AnalyseMood.java
@@ -1,10 +1,34 @@
 package com.bridgelabz;
 
 public class AnalyseMood {
+    String message;
+    public AnalyseMood(String message){
+        this.message = message;
+    }
+    AnalyseMood(){
+
+    }
     public String happyOrSad(String message) {
         if(message.contains("Sad")){
             return "SAD";
         }else{
+            return "HAPPY";
+        }
+    }
+
+    public String happyOrSadUsingConstructor() {
+        if(message.contains("happy")){
+            return "SAD";
+        }else{
+            return "HAPPY";
+        }
+    }
+
+    public String containsHappyGiveSad() {
+        if (message.contains("happy")){
+            return "SAD";
+        }
+        else {
             return "HAPPY";
         }
     }
--- src/test/java/com/bridgelabz/AnalyseMoodTest.java
+++ src/test/java/com/bridgelabz/AnalyseMoodTest.java
@@ -15,4 +15,16 @@ public class AnalyseMoodTest {
         String res = mood.happyOrSad("Sad");
         Assertions.assertEquals("SAD",res);
     }
+    @Test
+    public void checkMessageAndReturnMoodHappyUsingConstructor(){
+        AnalyseMood analyseMood = new AnalyseMood("I am in Sad mood");
+        String res = analyseMood.happyOrSadUsingConstructor();
+        Assertions.assertEquals("SAD",res);
+    }
+    @Test
+    public void messageContainsHappyShouldReturnSad(){
+        AnalyseMood analyseMood = new AnalyseMood("I am in happy mood");
+        String res = analyseMood.containsHappyGiveSad();
+        Assertions.assertEquals("SAD",res);
+    }
 }
